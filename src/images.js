e6711fb9452ee1684688310836585b9a2a124a07
ibrahima92 <ndaw43@gmail.com>
2020-03-23 17:35:50 +0000

    first and las commit

---
+++ src/images.js
@@ -0,0 +1,14 @@
+export default [
+    {
+     src: 'https://drive.google.com/uc?id=1_oA9Sx4D4DhFrYBFQdL0I1CUIz_LhQue',
+     text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse'
+    },
+    {
+     src: 'https://drive.google.com/uc?id=1rJFs-8So16UCiDag__hG4yyf_RnC08Fa',
+     text: 'Consectetur adipisicing elit cillum dolore eu fugiat nulla'
+    },
+    {
+     src: 'https://drive.google.com/uc?id=1HO2AGjd_1yyYI4pYTTBmGXBaWHoGSqCl',
+     text: 'Asperiores ex animi explicabo cillum dolore eu fugiat nulla'
+    }
+]
